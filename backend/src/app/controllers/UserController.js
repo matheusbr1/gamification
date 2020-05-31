33d20dc0cc5185ecd58750c0c2eac19c0d39f32f
Matheus Baron <matheusbaron10@gmail.com>
2020-05-31 15:10:50 -0300

    User's methods incremented

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -10,7 +10,7 @@ class UserController {
 
         // check if email exists
         if (emailExists) {
-            return response.json({ message: 'This email is already used' })
+            return response.status(400).json({ message: 'This email is already used' })
         }
 
         const user = await User.create({
@@ -24,6 +24,90 @@ class UserController {
             message: 'User created sucessfuly',
             user
         })
+    }
+
+    async list(request, response) {
+        const users = await User.findAll()
+        return response.json(users)
+    }
+
+    async listByIndex(request, response) {
+        const { index } = request.params
+
+        const user = await User.findOne({
+            where: { id: index }
+        })
+
+        if (!user) {
+            return response.status(404).json({ error: 'User not found' })
+        }
+
+        return response.json(user)
+    }
+
+    async update(request, response) {
+        const { name, email, oldPassword, password } = request.body
+        const { index } = request.params
+
+        const user = await User.findByPk(index)
+
+        // Check Index
+        if (!user) {
+            return response.status(404).json({ error: 'User not found' })
+        }
+
+        // check email
+        if (email !== user.email) {
+
+            // Check if new email already exists
+            const emailExists = await User.findOne({
+                where: { email }
+            })
+
+            // check if email exists
+            if (emailExists) {
+                return response.status(400).json({ error: 'This email is already used' })
+            }
+        }
+
+        // check password
+        if (oldPassword && (oldPassword !== user.password_hash)) {
+            return response.status(401).json({ error: 'Password does not match' })
+        }
+
+        if (oldPassword) {
+            const userUpdated = await user.update({
+                name,
+                email,
+                password_hash: password
+            })
+        }
+
+        // Update User
+        const userUpdated = await user.update(request.body)
+
+        return response.json({
+            message: 'User updated sucessfuly',
+            userUpdated
+        })
+    }
+
+    async delete(request, response) {
+        const { index } = request.params
+
+        const user = await User.findByPk(index)
+
+        if (!user) {
+            return response.status(404).json({ error: 'User not found' })
+        }
+
+        // Verify permission for delete any user
+
+        const userDeleted = await user.destroy({
+            where: { id: index }
+        })
+
+        return response.json({ message: 'User Deleted sucessfuly', userDeleted })
     }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,65 +9,22 @@ routes.get('/', (request, response) => {
     return response.json({ message: "Welcome" })
 })
 
-// Create User with Sequelize test
+// Create User
 routes.post('/users', UserController.store)
 
-// Users Array
-const users = [
-    {
-        name: "Pedro",
-        email: 'pedro@gmail.com',
-        password: "159756"
-    }
-]
-
 // List users
-routes.get('/users', (request, response) => {
-    return response.json({ users })
-})
+routes.get('/users', UserController.list)
 
 // List users by index
-routes.get('/users/:index', (request, response) => {
-
-    const { index } = request.params
-
-    const user = users[index]
-
-    if (!user) {
-        response.statusCode = 404
-        return response.json({ error: 'User not found' })
-    }
-
-    return response.json(user)
-})
+routes.get('/users/:index', UserController.listByIndex)
 
 // Update User 
-routes.put('/users/:index', (request, response) => {
-    const { name, email, password } = request.body
+routes.put('/users/:index', UserController.update)
 
-    const { index } = request.params
+// Delete User
+routes.delete('/users/:index', UserController.delete)
 
-    const user = {
-        name,
-        email,
-        password
-    }
-
-    users[index] = user
-
-    return response.json(users)
-})
-
-// Delete
-routes.delete('/users/:index', (request, response) => {
-
-    const { index } = request.params
-
-    users.splice(index, 1)
-
-    return response.json(users)
-})
-
+// Create Challenge
 routes.post('/challenges', ChallangeController.store)
 
 export default routes
